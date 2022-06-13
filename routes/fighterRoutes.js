454339b988b3b35bcb9c7d1954a743893070af81
Artyom <sudakov.artyom@gmail.com>
2022-06-13 19:17:22 +0300

    added crud(read) methods fot fighter

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,10 +1,43 @@
-const { Router } = require('express');
-const FighterService = require('../services/fighterService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
-const { createFighterValid, updateFighterValid } = require('../middlewares/fighter.validation.middleware');
+const { Router } = require("express");
+const FighterService = require("../services/fighterService");
+const { responseMiddleware } = require("../middlewares/response.middleware");
+const {
+	createFighterValid,
+	updateFighterValid,
+} = require("../middlewares/fighter.validation.middleware");
 
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+
+router.get(
+	"/",
+	async (req, res, next) => {
+		try {
+			const users = await FighterService.findAll();
+			res.data = users;
+			next();
+		} catch (err) {
+			console.log(err);
+			return res.status(400).json({ message: "Fighter not found" });
+		}
+	},
+	responseMiddleware
+);
+
+router.get(
+	"/:id",
+	async (req, res, next) => {
+		try {
+			const fighter = await FighterService.search({ id: req.params.id });
+			res.data = fighter;
+			next();
+		} catch (err) {
+			console.log(err);
+			return res.status(400).json({ message: "User id not found" });
+		}
+	},
+	responseMiddleware
+);
 
 module.exports = router;
--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,24 @@
-const { FighterRepository } = require('../repositories/fighterRepository');
+const { FighterRepository } = require("../repositories/fighterRepository");
 
 class FighterService {
-    // TODO: Implement methods to work with fighters
+	// TODO: Implement methods to work with fighters
+
+	findAll() {
+		const items = FighterRepository.getAll();
+
+		if (!items) {
+			throw Error("Fighter don't found");
+		}
+		return items;
+	}
+
+	search(search) {
+		const item = UserRepository.getOne(search);
+		if (!item) {
+			return new Error({ message: "Not found fighter" });
+		}
+		return item;
+	}
 }
 
 module.exports = new FighterService();
