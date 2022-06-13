636cb841157306647cf0b559808a6b6ecdde47b5
Artyom <sudakov.artyom@gmail.com>
2022-06-13 14:20:53 +0300

    Implemented login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,24 @@
-const { Router } = require('express');
-const AuthService = require('../services/authService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const AuthService = require("../services/authService");
+const { responseMiddleware } = require("../middlewares/response.middleware");
 
 const router = Router();
 
-router.post('/login', (req, res, next) => {
-    try {
-        // TODO: Implement login action (get the user if it exist with entered credentials)
-        res.data = data;
-    } catch (err) {
-        res.err = err;
-    } finally {
-        next();
-    }
-}, responseMiddleware);
+router.post(
+	"/login",
+	async (req, res, next) => {
+		try {
+			// TODO: Implement login action (get the user if it exist with entered credentials)
+			const { email, password } = req.body;
+			const data = await AuthService.login({ email, password });
+			res.data = data;
+		} catch (err) {
+			res.err = err;
+		} finally {
+			next();
+		}
+	},
+	responseMiddleware
+);
 
 module.exports = router;
